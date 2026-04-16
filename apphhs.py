9fb16528dbcc56b10b95b9ddd6e6e4a1f9af9ae5
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-04-16 10:52:17 +0530

    Update apphhs.py

--- apphhs.py
+++ apphhs.py
@@ -2,7 +2,8 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel('/content/sample_data/HHS_Unaccompanied_Alien_Children_Program.xlsx')
+# df = pd.read_excel('/content/sample_data/HHS_Unaccompanied_Alien_Children_Program.xlsx')
+df = pd.read_excel('HHS_Unaccompanied_Alien_Children_Program.xlsx', engine = 'openpyxl')
 st.title("Hello from Colab via ngrok")
 st.write("This works!")
 st.dataframe(df)
