c0c3f1852041975ccf2aac1ce90ead80053e694b
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-26 13:31:12 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -46,12 +46,13 @@ if os.path.exists(image_path1) & os.path.exists(image_path2):
     img2 = img2.resize((50,50))
  
     # st.image([img1,img2] use_column_width=False)
-    col1, col2 = st.columns(2, gap="small")
+    col1, col2, col3 = st.columns(3, gap="small")
     with col1:
         st.image(img1, use_column_width=True)
     with col2:
         st.image(img2, use_column_width=True)
-
+    with col3:
+        pass
 else:
     st.error(f"Image not found: {image_path}")
 #--------------------------------------
