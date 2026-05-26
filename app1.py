627bbb901bbde35181894427df81804c2c5669a1
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-26 15:07:52 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -52,7 +52,7 @@ if os.path.exists(image_path1) & os.path.exists(image_path2):
     with col2:
         st.image(img2, use_column_width=True)
     with col3:
-        st.write("Health Care Data Analysis '\n' Helps to Understand the Present Data & '\n' Predict The Future Situation Auding Thd Healthcare Personnel to Plan Ahead To take necessary steps")
+        st.write("Health Care Data Analysis \n Helps to Understand the Present Data & \n Predict The Future Situation & Aiding The Healthcare Personnel to Plan Ahead To take necessary steps")
 else:
     st.error(f"Image not found: {image_path}")
 #--------------------------------------
