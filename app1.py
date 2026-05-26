1f1480086c912635612619200df872ae29c25d4a
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-26 13:27:05 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -41,9 +41,9 @@ image_path2 = "graph_bar-chart.jpeg"
 
 if os.path.exists(image_path1) & os.path.exists(image_path2):
     img1 = Image.open(image_path1)
-    img1 = img1.resize((70, 70))
+    img1 = img1.resize((50, 50))
     img2 = Image.open(image_path2)
-    img2 = img2.resize((70,70))
+    img2 = img2.resize((50,50))
  
     # st.image([img1,img2] use_column_width=False)
     col1, col2 = st.columns(2, gap="small")
