877dd53587763d3c0a822a722fdba550b381bb22
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-26 15:28:28 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -792,7 +792,9 @@ if st.button(
                 )
 
     else:
-        st.write("Train Model First & Check All Values Submitted & Click Predict2")
+        st.write("Train Model First &")
+        st.write("Check All Values Submitted &")
+        st.write("Click Predict2")
         for err in error_messages:
             #st.write("Please Train Model")
             st.error(err)
