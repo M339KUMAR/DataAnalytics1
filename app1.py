39617e221cb989392c923b6b453fca3aa81ff7f9
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-26 11:52:30 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -38,6 +38,7 @@ image_path = "Scope.jpeg"
 
 if os.path.exists(image_path):
     img = Image.open(image_path)
+    img = img.resize((50, 50))
     st.image(img, use_column_width=True)
 else:
     st.error(f"Image not found: {image_path}")
