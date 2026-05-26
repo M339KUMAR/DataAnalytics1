44995a40e8f7ee01f9c78a82a54e25aa209f46c6
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-05-26 13:55:01 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -636,7 +636,7 @@ if st.button("Predict1"):
                                   'Children in HHS Care':[CinHHS],
                                   'Children discharged from HHS Care':[CdHHS]
                                  })
-     try:
+try:
        if "results" in st.session_state:
            forecast = st.session_state.results.forecast(
                 steps=1,
@@ -654,8 +654,8 @@ if st.button("Predict1"):
            "Predicted Total Load",
            f"{predicted_value:,.2f}"
         )
-     except Exception:
-      print("Train The Model - using Train Model Button")
+except Exception:
+    print("Train The Model - using Train Model Button")
 #st.write(results.summary())
 
 st.write("------------------------------------------")
