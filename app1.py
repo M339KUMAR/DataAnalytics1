1b0f067c114faeaab001386c781e40868eb1147a
M339KUMAR <147836243+M339KUMAR@users.noreply.github.com>
2026-07-13 17:27:05 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -91,9 +91,9 @@ if os.path.exists(image_path1) & os.path.exists(image_path2):
     # st.image([img1,img2] use_column_width=False)
     col1, col2, col3 = st.columns(3, gap="small")
     with col1:
-        st.image(img1, use_column_width=True)
+        st.image(img1, use_container_width=True)#use_column_width
     with col2:
-        st.image(img2, use_column_width=True)
+        st.image(img2, use_container_width=True)#use_column_width
     with col3:
         st.write("Health Care Data Analysis \n Helps to Understand the Present Data & \n Predict The Future Situation & Aiding The Healthcare Personnel to Plan Ahead To take necessary steps")
 else:
